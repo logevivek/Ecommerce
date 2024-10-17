fa0b3bd59fe804c98e01499e1b4b28f69653c6bf
logevivek <vivek.logelite@gmail.com>
2024-10-17 17:24:50 +0530

    fhghfgsdgsdgsdgsdgd

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -10,7 +10,7 @@ use App\Models\Category;
 class CategoryController extends Controller
 {
     public function ProductCategory(){
-      ///hsdghsdhsdhghsdghsdhsdhd
+      //jhfjksdhfjkhsdfjkhsdjkfhsdjkhfjksdhfjkdshfjksdhfjkhdsfjkdhsjkfhsd
         $categories = Category::orderBy('id', 'desc')
         ->where('trash',0)
         ->get();
